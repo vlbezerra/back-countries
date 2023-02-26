afb77687d4f74a2c0f8fea693086071cdc32375e
Vinícius Bezerra <vinicius.bezerra@bs.nttdata.com>
2023-02-26 16:33:13 -0300

    feat: version 2

--- src/handlers/LogHandler.ts
+++ src/handlers/LogHandler.ts
@@ -9,14 +9,6 @@ import { CountriesService } from "../entities/entities";
  */
 @Handler(CountriesService.SanitizedEntity.Logs)
 export class LogsHandler {
-    // You can also decorate multiple methods with the same docorator (@BeforRead in this example)
-    // This way you can structure the code which should be executed on the beforRead hook and break it down into smaller pieces
-    @OnCreate()
-    public async createLog(@ParamObj() logs: CountriesService.Logs): Promise<CountriesService.Logs>{
-        return logs;
-    }
-    @OnRead()
-    public async getLog() {}
     @BeforeRead()
     public async checkAuth() {}
     @BeforeRead()
